714b71dcb14b0fb408105a823ffda00ad777b535
markanday sahu <78273939+MarkandaySahu@users.noreply.github.com>
2025-02-05 01:07:34 +0530

    added enemy

---
+++ Enemy.lua
@@ -0,0 +1,50 @@
+local love = require"love"
+
+function Enemy()
+    local dice = math.random(1,4)--determine from which side enemy will pop up.
+    local _x,_y
+    local _radius = 20
+
+    if dice==1 then
+        _x=math.random(0,love.graphics.getWidth())
+        _y=-_radius*4
+    elseif dice==2 then
+        _x=-_radius*4
+        _y=math.random(0,love.graphics.getHeight())
+    elseif dice==3 then
+        _x=math.random(0,love.graphics.getWidth())
+        _y=love.graphics.getHeight() + (_radius*4)
+    elseif dice==4 then
+        _x=love.graphics.getWidth() + (_radius*4)
+        _y=math.random(0,love.graphics.getWidth())
+    end
+
+    return{
+        level=1,
+        radius=_radius,
+        x=_x,
+        y=_y,
+
+        move = function (self,player_x,player_y)
+            --for x coordinate
+            if player_x - self.x >0 then--enemy is at the left side of the player
+                self.x = self.x + self.level--move the enemy a level closer to player
+            elseif player_x - self.x <0 then--enemy is at the Right side of the player
+                self.x = self.x - self.level--move the enemy a level closer to player
+            end
+            --for y coordinate
+            if player_y - self.y >0 then--enemy is at the upper side of the player
+                self.y = self.y + self.level--move the enemy a level closer to player
+            elseif player_y - self.y <0 then--enemy is at the lower side of the player
+                self.y = self.y - self.level--move the enemy a level closer to player
+            end
+        end,
+        draw = function (self)
+            love.graphics.setColor(1,0.5,0.7) 
+            love.graphics.circle("fill",self.x,self.y,self.radius)
+            love.graphics.setColor(1,1,1)
+        end
+    }
+end
+
+return Enemy
---
+++ conf.lua
@@ -0,0 +1,6 @@
+local love = require"love"
+function love.conf(t)
+    t.window.height=1000
+    t.window.width=1920
+    t.window.icon="wd.jpg"
+end
